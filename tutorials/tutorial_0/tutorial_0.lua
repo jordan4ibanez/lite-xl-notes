42a659dd1138b9b746e5379529047f9d8ea34d34
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-11 17:58:16 -0400

    Add tutorial 0

---
+++ tutorials/tutorial_0/tutorial_0.lua
@@ -0,0 +1,20 @@
+-- mod-version:3
+
+--[[
+
+This is a module. Very fancy I know.
+
+A plugin can just be a lua file. Like this one!
+
+Why am I calling it a module and a plugin? You'll learn this in the next tutorial!
+
+Also, you just drop this file into your lite-xl/data/plugins/ folder and it auto loads. Yay.
+
+Oh right, and that thing at the top, that's not just a comment, but you'll learn about this in the next tutorial.
+
+I suppose, this is the entirety of a basic plugin, this runs, so it's a plugin. It just doesn't really do anything.
+
+But we can't just leave it off on this comment. Let's do, at least something.
+]]--
+
+print("I am a plugin. Fear me!")
