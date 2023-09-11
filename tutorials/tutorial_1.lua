93a121627b38188eaf62153711859c1dcca9b1a6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-11 14:46:06 -0400

    Document colors

--- tutorials/tutorial_1.lua
+++ tutorials/tutorial_1.lua
@@ -15,7 +15,7 @@ I know this can look a bit overwhelming, but if you read through it, you'll unde
 Now, let's get to some basics.
 
 
---------------------------------------------------------
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
 Q: What is a module?
@@ -53,7 +53,7 @@ As you can see, we've been localizing all these imported modules so they don't l
 Now, let me explain a bit of info about why we're going to import rootview:
 
 
------------------------------------------------------
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
 RootView is a View that manages all the other Views.
@@ -84,13 +84,78 @@ If you wanted to make a dependency on another plugin, it's quite simple.
 
 local MyModule = require "plugins.my_module"
 
-
 Pretty simple, right? this imported module would just be called "my_module.lua" in the plugins folder.
 
-Don't worry, we'll touch up on this later in the tutorial. Let's focus a bit on this one for now.
+
+Something I want you to notice: Now we're using the words "plugin" and "module" interchangeably!
+
+This is because they use the same scheme. A plugin IS a module.
+
+
+Don't worry, we'll touch up on this later in the tutorial. And if I forgot to, call me out on it.
+
+Let's focus on this one for now.
+
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+We're going to make a configuration for this plugin.
+
+We imported the config module and that's what we use to store per-plugin settings, pretty neat huh?
+
+So it's utilized by creating a table in config.plugins.your_plugin_name. Where your_plugin_name is the name of the plugin.
+
+Since this plugin is called tutorial_1, we can just do this:
 
 ]]--
+config.plugins.tutorial_1 = {}
 
+
+--[[
+
+Colors. Everyone loves colors! You're looking at a bunch of colors right now.
+
+Q: But how do we use colors in lite-xl?
+
+A: There are 2 possible ways to do this! Both quite simple. Let's see below.
+
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+1: Literals
+
+Literals are RGB or RGBA formatted integral (whole number) tables. Internally, it utilizes UBYTE. This means 0-255 are valid input on each element.
+
+So what am I talking about? Well, let me show you:
+
+Red
+config.plugins.tutorial_1.text_color = { 255, 0, 0 }
+
+Translucent Gray
+config.plugins.tutorial_1.text_color = { 127, 127, 127, 127 }
+
+
+As you can see, it's quite simple. The alpha channel in Red Blue Green Alpha is optional! Automatically gets set to 255 if left out.
+
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+2: Hex
+
+If you want to use hex literals, it's also quite simple. Just import common like so:
+
+local common = require "core.common"
+
+A note for the format. Just like literals, there are two options: RRGGBB and RRGGBBAA. AA stands for hex alpha channel
+
+Red
+config.plugins.tutorial_1.text_color = { common.color "#FF0000" }
+
+Translucent Gray
+config.plugins.tutorial_1.text_color = { common.color "#80808080" } <- that last 80 is the alpha channel :) 50%
+
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+We're going to use violet because it's quite visible:
+
+]]--
 
 
 
